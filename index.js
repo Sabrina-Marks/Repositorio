6a3cf21b8e569c5a1681a2b3870ff6d6bbc21634
Sabrina Marques <sabrinamarques0240@gmail.com>
2024-02-20 11:07:46 -0300

    Atualização do js e css

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const $startGameButton = document.querySelector(".start-quiz")
 const $questionsContainer = document.querySelector(".question-container")
 const $answerContainer = document.querySelector(".answer-container")
 const $questionText = document.querySelector(".question")
-
+const $nextQuestionButton = document.querySelector(".next-question")
 
 $startGameButton.addEventListener("click", iniciarJogo)
-
+$nextQuestionButton.addEventListener("click", displayNextQuestion)
 
 let curretQuestionIndex = 0
+let totalCorrect = 0
 
 function iniciarJogo() {
    $startGameButton.classList.add("hide")
@@ -16,9 +17,11 @@ function iniciarJogo() {
 }
 
 function displayNextQuestion() {
-while($answerContainer.firstChild) {
-   $answerContainer.removeChild($answerContainer.firstChild)
+resetState()
+if (questions.length === curretQuestionIndex){
+    return finishGame()
 }
+
 $questionText.textContent = questions[curretQuestionIndex].question
 questions[curretQuestionIndex].answer.forEach(answer => {
     const newAnswer = document.createElement("button")
@@ -28,25 +31,36 @@ questions[curretQuestionIndex].answer.forEach(answer => {
         newAnswer.dataset.correct = answer.correct
     }
     $answerContainer.appendChild(newAnswer)
-    
+
     newAnswer.addEventListener("click", selectAnswer)
 })
 }
 
-function selectAnswer(event){
+function resetState() {
+    while($answerContainer.firstChild) {
+        $answerContainer.removeChild($answerContainer.firstChild)
+     }
+     
+     document.body.removeAttribute("class")
+     $nextQuestionButton.classList.add("hide")
+
+}
+
+function selectAnswer(event) {
     const answerClicked = event.target
 
     if(answerClicked.dataset.correct){
         document.body.classList.add("correct")
+        totalCorrect++
     } else{
         document.body.classList.add("incorrect")
     }
 
     document.querySelectorAll(".answer").forEach(button => {
         if(button.dataset.correct){
-            button.classList.add(correct)
+            button.classList.add("correct")
         } else{
-            button.classList.add(incorrect)
+            button.classList.add("incorrect")
         }
 
         button.disabled = true
@@ -54,14 +68,41 @@ function selectAnswer(event){
     } )
 
 
-
-
-
-
-
-
+ $nextQuestionButton.classList.remove("hide")
+ curretQuestionIndex++
 }
 
+function finishGame() {
+    const totalQuestion = questions.length
+    const performance = Math.floor(totalCorrect *100 / totalQuestion)
+
+    let message = " "
+
+    switch(true){
+        case(performance >= 90):
+        message = "Excelente!! :)"
+        break
+        case(performance >= 70):
+        message = "Muito bom :)"
+        break
+        case (performance >=50 ):
+        message = "Bom"
+        break
+        default:
+        message = "Tente novamente:("
+    }
+    $questionsContainer.innerHTML = 
+    ` 
+    <p class="final-message">
+    Você acertou ${totalCorrect} de ${totalQuestion}
+    <span> Resultado ${message}</span>
+    </p>
+    <button onclick=window.location.reload() class="button"> 
+    Refazer teste
+    </button>
+    `
+
+}
 
 const questions = [
    {
