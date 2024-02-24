519c90fe3b07dc4d0cbdb400cea693949dc64e19
Sabrina Marques <sabrinamarques0240@gmail.com>
2024-02-24 09:39:59 -0300

    Possiveis mudanças de perguntas

--- index.js
+++ index.js
@@ -105,67 +105,67 @@ function finishGame() {
 }
 
 const questions = [
-   {
-       question: "Qual era o nome original do grupo Fifth Harmony?",
-       answer: [ 
-        { text: "Fab Five", correct: false},
-        { text: "Lylas", correct: true},
-        { text: "Harmonize", correct: false} ,
-        { text: "Girl Power", correct: false},
+    {
+        question: "Qual era o nome original do grupo Fifth Harmony?",
+        answer: [ 
+         { text: "Fab Five", correct: false},
+         { text: "Lylas", correct: true},
+         { text: "Harmonize", correct: false} ,
+         { text: "Girl Power", correct: false},
+       ],
+    },
+    {
+        question : "Qual das integrantes do Fifth Harmony saiu do grupo em 2016?",
+        answer: [
+         { text: "Camila Cabello", correct: true},
+         { text: "Lauren Jauregui", correct: false},
+         { text: "Normani Kordei", correct: false},
+         { text: "Ally Brooke", correct: false},
       ],
-   },
-   {
-       question : "Qual das integrantes do Fifth Harmony saiu do grupo em 2016?",
+    },
+    {
+       question: "Qual música do Fifth Harmony alcançou o primeiro lugar nas paradas da Billboard?",
        answer: [
-        { text: "Camila Cabello", correct: true},
-        { text: "Lauren Jauregui", correct: false},
-        { text: "Normani Kordei", correct: false},
-        { text: "Ally Brooke", correct: false},
-     ],
-   },
-   {
-      question: "Qual música do Fifth Harmony alcançou o primeiro lugar nas paradas da Billboard?",
-      answer: [
-       { text: "Work from Home", correct: false}, 
-       { text: "Worth It", correct: true},
-       { text: "All In My Head (Flex)", correct: false},
-       { text: "Down", correct: false},
-       ],
-   },
-   {
-        question : "Em que programa de televisão o Fifth Harmony se formou?",
-        answer : [
-            { text: "The Voice", correct: false},
-            { text: "The X Factor", correct: true}, 
-            { text: "American Idol", correct: false}, 
-            { text: "America's Got Talent", correct: false},
-       ],
-   },
-   {
-        question : "Quantas integrantes originais havia no Fifth Harmony?",
-        answer: [
-            { text: "5", correct: true}, 
-            { text: "6", correct: false}, 
-            { text: "4", correct: false}, 
-            { text: "3", correct: false},
-       ],
-   },
-   {
-        question: "Qual integrante do Fifth Harmony foi finalista do programa Dancing with the Stars?",
-        answer: [
-            { text: "Camila Cabello", correct: false},
-            { text: "Lauren Jauregui", correct: false},
-            { text: "Normani Kordei", correct: true},  
-            { text: "Dinah Jane", correct: false},
-       ],
-   },
-   {
-        question : "Em que ano o Fifth Harmony anunciou seu hiato indefinido?",
-        answer: [
-            { text: "2017", correct: true},
-            { text: "2018", correct: false}, 
-            { text: "2019", correct: false},
-            { text: "2020", correct: false}, 
-       ],
-   },
-]
+        { text: "Work from Home", correct: false}, 
+        { text: "Worth It", correct: true},
+        { text: "All In My Head (Flex)", correct: false},
+        { text: "Down", correct: false},
+        ],
+    },
+    {
+         question : "Em que programa de televisão o Fifth Harmony se formou?",
+         answer : [
+             { text: "The Voice", correct: false},
+             { text: "The X Factor", correct: true}, 
+             { text: "American Idol", correct: false}, 
+             { text: "America's Got Talent", correct: false},
+        ],
+    },
+    {
+         question : "Quantas integrantes originais havia no Fifth Harmony?",
+         answer: [
+             { text: "5", correct: true}, 
+             { text: "6", correct: false}, 
+             { text: "4", correct: false}, 
+             { text: "3", correct: false},
+        ],
+    },
+    {
+         question: "Qual integrante do Fifth Harmony foi finalista do programa Dancing with the Stars?",
+         answer: [
+             { text: "Camila Cabello", correct: false},
+             { text: "Lauren Jauregui", correct: false},
+             { text: "Normani Kordei", correct: true},  
+             { text: "Dinah Jane", correct: false},
+        ],
+    },
+    {
+         question : "Em que ano o Fifth Harmony anunciou seu hiato indefinido?",
+         answer: [
+             { text: "2017", correct: true},
+             { text: "2018", correct: false}, 
+             { text: "2019", correct: false},
+             { text: "2020", correct: false}, 
+        ],
+    },
+ ]
